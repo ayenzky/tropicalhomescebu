f690a9f65d6b43b14d0a8e03797154031ff802d6
allen <allen@webriq.services>
2025-02-28 10:13:14 +0800

    add pwa

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'astro/config';
 import sanity from '@sanity/astro';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
-
+import pwa from '@vite-pwa/astro';
 import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
@@ -18,7 +18,78 @@ export default defineConfig({
       studioBasePath: '/studio' // If you want to access the Studio on a route
     }), 
     react(),
-    tailwind()
+    tailwind(),
+    pwa({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Tropical Homes Cebu',
+        short_name: 'TropicalHomesCebu',
+        description: 'Tropical Homes Cebu',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        "icons": [
+          {
+            "src": "icons/icon-48x48.png",
+            "sizes": "48x48",
+            "type": "image/png"
+          },
+          {
+            "src": "icons/icon-72x72.png",
+            "sizes": "72x72",
+            "type": "image/png"
+          },
+          {
+            "src": "icons/icon-96x96.png",
+            "sizes": "96x96",
+            "type": "image/png"
+          },
+          {
+            "src": "icons/icon-128x128.png",
+            "sizes": "128x128",
+            "type": "image/png"
+          },
+          {
+            "src": "icons/icon-144x144.png",
+            "sizes": "144x144",
+            "type": "image/png"
+          },
+          {
+            "src": "icons/icon-152x152.png",
+            "sizes": "152x152",
+            "type": "image/png"
+          },
+          {
+            "src": "icons/icon-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          },
+          {
+            "src": "icons/icon-256x256.png",
+            "sizes": "256x256",
+            "type": "image/png"
+          },
+          {
+            "src": "icons/icon-384x384.png",
+            "sizes": "384x384",
+            "type": "image/png"
+          },
+          {
+            "src": "icons/icon-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+          }
+        ],
+      },
+      workbox: {
+        navigateFallback: '/',
+        globPatterns: ['**/*.{css,js,html,svg,png,ico,jpg,jpeg}'],
+      },
+      devOptions: {
+        enabled: true,
+        navigateFallbackAllowlist: [/^\/$/],
+      },
+    })
   ],
   adapter: netlify()
 });
